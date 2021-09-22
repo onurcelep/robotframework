403e9456e7dc1de9fbd0e4d11a47ec711cc74678
Pekka Klärck <peke@iki.fi>
2021-09-22 22:40:22 +0300

    Remove Python 2, Jython and IronPython tests and test support code.
    Test files only testing these not anymore supported interpreters,
    including libraries, were removed already in a separate commit.
    
    Part of #3457.
    
    Also some whitespace cleanup here and there.

--- utest/utils/test_compat.py
+++ utest/utils/test_compat.py
@@ -9,7 +9,6 @@ from robot.utils.asserts import assert_equal, assert_false, assert_raises
 class TestIsATty(unittest.TestCase):
 
     def test_with_stdout_and_stderr(self):
-        # file class based in PY2, io module based in PY3
         assert_equal(isatty(sys.__stdout__), sys.__stdout__.isatty())
         assert_equal(isatty(sys.__stderr__), sys.__stderr__.isatty())
 
